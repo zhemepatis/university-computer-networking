c9d7501130546df43c2b697d4a09925926e38231
zhemepatis <rink.gab@gmail.com>
2024-03-13 11:22:03 +0200

    insignificant changes to single-client -server

--- single-client-server/src/client.c
+++ single-client-server/src/client.c
@@ -1,10 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <string.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define PORT "9002"
 #define HOST "::1"
--- single-client-server/src/server.c
+++ single-client-server/src/server.c
@@ -1,12 +1,14 @@
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 #include <string.h>
 #include <ctype.h>
 #include <regex.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/sendfile.h>
